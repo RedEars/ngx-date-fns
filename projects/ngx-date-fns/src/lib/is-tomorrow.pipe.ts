a1df56a8fe2debc55375afa9cdfa0d0357966e53
Sydney Pollak <sydney.p@me.com>
2020-11-03 23:36:44 +0100

    Add isTomorrow Pipe

---
+++ projects/ngx-date-fns/src/lib/is-tomorrow.pipe.ts
@@ -0,0 +1,16 @@
+import { NgModule, Pipe, PipeTransform } from '@angular/core';
+import { DateFnsInputDate } from './types';
+import isToday from 'date-fns/isTomorrow';
+
+@Pipe({ name: 'dfnsIsTomorrow' })
+export class IsTomorrowPipe implements PipeTransform {
+  transform(date: DateFnsInputDate): boolean {
+    return isTomorrow(date);
+  }
+}
+
+@NgModule({
+  declarations: [IsTomorrowPipe],
+  exports: [IsTomorrowPipe],
+})
+export class IsTomorrowPipeModule {}
